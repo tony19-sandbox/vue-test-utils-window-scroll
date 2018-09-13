5766f4cc0c13d4842559bc053f6d17840af1952e
Tony Trinh <tony19@gmail.com>
2018-09-13 00:24:17 -0500

    Demo window.scroll unit test

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,13 +1,36 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { expect } from 'chai';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+
+  afterEach(() => {
+    window.pageYOffset = 0;
+  });
+
+  it('top banner is sticky when scrolled past top', async () => {
+    const localVue = createLocalVue();
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
-})
+      localVue,
+      attachToDocument: true,
+    });
+    const dBanner = wrapper.find('#topB')
+    window.pageYOffset = 2000;
+
+    wrapper.vm.topBResize();
+    expect(dBanner.classes()).to.contain('sticky')
+  });
+
+  it('top banner not sticky when scrolled below top', async () => {
+    const localVue = createLocalVue();
+    const wrapper = shallowMount(HelloWorld, {
+      localVue,
+      attachToDocument: true,
+    });
+    const dBanner = wrapper.find('#topB')
+    window.pageYOffset = 0;
+
+    wrapper.vm.topBResize();
+    expect(dBanner.classes()).to.not.contain('sticky')
+  });
+});
